21029ad8b3b166792540779a671c061960f1f7fe
Alex Woods <alexwoo@amazon.com>
2024-12-23 15:21:04 -0800

    Do the same for polish spec

--- gems/smithy/spec/interfaces/polish_spec.rb
+++ gems/smithy/spec/interfaces/polish_spec.rb
@@ -1,29 +1,29 @@
 # frozen_string_literal: true
 
 describe 'Types: Polishing' do
-  Class.new(Smithy::Polish) do
-    def polish(artifact)
-      file, _content = artifact.find { |file, _content| file.include?('/types.rb') }
-      inject_into_module(file, 'Types') do
-        "    Polish = Struct.new(keyword_init: true)\n"
-      end
-    end
-  end
-
-  Class.new(Smithy::Polish) do
-    def for?(_model)
-      false
-    end
-
-    def polish(artifact)
-      file, _content = artifact.find { |file, _content| file.include?('/types.rb') }
-      inject_into_module(file, 'Types') do
-        "    ShouldNotExist = Struct.new(keyword_init: true)\n"
-      end
-    end
-  end
-
   before(:all) do
+    # Define Polish classes (scoped to this block only)
+    Class.new(Smithy::Polish) do
+      def polish(artifact)
+        file, _content = artifact.find { |file, _content| file.include?('/types.rb') }
+        inject_into_module(file, 'Types') do
+          "    Polish = Struct.new(keyword_init: true)\n"
+        end
+      end
+    end
+
+    Class.new(Smithy::Polish) do
+      def for?(_model)
+        false
+      end
+
+      def polish(artifact)
+        file, _content = artifact.find { |file, _content| file.include?('/types.rb') }
+        inject_into_module(file, 'Types') do
+          "    ShouldNotExist = Struct.new(keyword_init: true)\n"
+        end
+      end
+    end
     @tmpdir = SpecHelper.generate(['Weather'], :types)
   end
 
