8f97cc1179e87d1ca3aa1fa3b10d4f4c3c2a510e
Matt Muller <53055821+mullermp@users.noreply.github.com>
2025-01-06 08:28:38 -0800

    Request example (#236)

--- gems/smithy/lib/smithy/vise.rb
+++ gems/smithy/lib/smithy/vise.rb
@@ -8,5 +8,29 @@ require_relative 'vise/structure_parser'
 
 module Smithy
   # A module that parses the Smithy JSON model.
-  module Vise; end
+  module Vise
+    PRELUDE_SHAPES = %w[
+      smithy.api#BigInteger
+      smithy.api#BigDecimal
+      smithy.api#Blob
+      smithy.api#Boolean
+      smithy.api#Byte
+      smithy.api#Document
+      smithy.api#Double
+      smithy.api#Float
+      smithy.api#Integer
+      smithy.api#Long
+      smithy.api#PrimitiveBoolean
+      smithy.api#PrimitiveByte
+      smithy.api#PrimitiveDouble
+      smithy.api#PrimitiveFloat
+      smithy.api#PrimitiveInteger
+      smithy.api#PrimitiveLong
+      smithy.api#PrimitiveShort
+      smithy.api#Short
+      smithy.api#String
+      smithy.api#Timestamp
+      smithy.api#Unit
+    ].freeze
+  end
 end
