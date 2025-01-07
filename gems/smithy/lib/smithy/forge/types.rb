bd60884d7e0ea5574208053db07f0ee8d99c37f1
Alex Woods <alexwoo@amazon.com>
2025-01-06 17:57:35 -0800

    Fix polish_spec warning - ensure rubocop is generated for types as well (#242)

--- gems/smithy/lib/smithy/forge/types.rb
+++ gems/smithy/lib/smithy/forge/types.rb
@@ -25,6 +25,7 @@ module Smithy
       def source_files
         Enumerator.new do |e|
           e.yield "#{@gem_name}.gemspec", Anvil::Client::Views::Gemspec.new(@plan).hammer
+          e.yield '.rubocop.yml', Anvil::Client::Views::Rubocop.new(@plan).hammer
           e.yield "lib/#{@gem_name}.rb", Anvil::Client::Views::Module.new(@plan).hammer
           e.yield "lib/#{@gem_name}/types.rb", Anvil::Client::Views::Types.new(@plan).hammer
         end
