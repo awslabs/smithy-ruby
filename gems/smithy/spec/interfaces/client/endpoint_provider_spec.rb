2bd8f874f35523c9cdbd479d02dab789981e0017
Alex Woods <alexwoo@amazon.com>
2025-01-08 10:49:38 -0800

    Revert testing spec

--- gems/smithy/spec/interfaces/client/endpoint_provider_spec.rb
+++ gems/smithy/spec/interfaces/client/endpoint_provider_spec.rb
@@ -1,45 +1,30 @@
 # frozen_string_literal: true
 
 describe 'Component: EndpointProvider' do
-  # before(:all) do
-  #   @tmpdir = SpecHelper.generate(['EndpointDefaults'], :client, fixture: 'endpoints/default-values')
-  # end
-  #
-  # after(:all) do
-  #   SpecHelper.cleanup(['EndpointDefaults'], @tmpdir)
-  # end
-  #
-  # subject { EndpointDefaults::EndpointProvider.new }
-  #
-  # describe '.resolve_endpoint' do
-  #   it 'resolves the endpoint' do
-  #     params = EndpointDefaults::EndpointParameters.new(bar: 'bar', baz: 'baz')
-  #
-  #     out = subject.resolve_endpoint(params)
-  #     expect(out).to be_a(Smithy::Client::EndpointRules::Endpoint)
-  #     expect(out.uri).to eq('https://example.com/baz')
-  #   end
-  #
-  #   it 'raises errors from rules' do
-  #     params = EndpointDefaults::EndpointParameters.new(bar: nil, baz: 'baz')
-  #     expect do
-  #       subject.resolve_endpoint(params)
-  #     end.to raise_error(ArgumentError, 'endpoint error')
-  #   end
-  # end
+  before(:all) do
+    @tmpdir = SpecHelper.generate(['EndpointDefaults'], :client, fixture: 'endpoints/default-values')
+  end
 
-  # TODO: Remove me, temporary for debugging
-  context 'endpoint-bindings' do
-    before(:all) do
-      @eptmpdir = SpecHelper.generate(['EndpointBindings'], :client, fixture: 'endpoints/endpoints-string-array')
+  after(:all) do
+    SpecHelper.cleanup(['EndpointDefaults'], @tmpdir)
+  end
+
+  subject { EndpointDefaults::EndpointProvider.new }
+
+  describe '.resolve_endpoint' do
+    it 'resolves the endpoint' do
+      params = EndpointDefaults::EndpointParameters.new(bar: 'bar', baz: 'baz')
+
+      out = subject.resolve_endpoint(params)
+      expect(out).to be_a(Smithy::Client::EndpointRules::Endpoint)
+      expect(out.uri).to eq('https://example.com/baz')
     end
 
-    after(:all) do
-      SpecHelper.cleanup(['EndpointBindings'], @eptmpdir)
-    end
-
-    it 'builds' do
-      EndpointBindings::Client.new
+    it 'raises errors from rules' do
+      params = EndpointDefaults::EndpointParameters.new(bar: nil, baz: 'baz')
+      expect do
+        subject.resolve_endpoint(params)
+      end.to raise_error(ArgumentError, 'endpoint error')
     end
   end
 end
