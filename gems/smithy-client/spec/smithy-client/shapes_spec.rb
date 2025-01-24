7ff009ceee4399e8d9582299d72e5b4b6f9c1b43
Matt Muller <mamuller@amazon.com>
2025-01-24 10:53:26 -0500

    Strengthen shapes spec

--- gems/smithy-client/spec/smithy-client/shapes_spec.rb
+++ gems/smithy-client/spec/smithy-client/shapes_spec.rb
@@ -14,6 +14,16 @@ module Smithy
           it 'defaults traits to nil' do
             expect(subject.traits).to be_nil
           end
+
+          it 'can set id' do
+            subject = Shape.new(id: 'foo')
+            expect(subject.id).to eq('foo')
+          end
+
+          it 'can set traits' do
+            subject = Shape.new(traits: { 'trait' => 'value' })
+            expect(subject.traits).to eq({ 'trait' => 'value' })
+          end
         end
       end
 
@@ -101,6 +111,11 @@ module Smithy
             subject.add_member(:foo, StringShape.new)
             expect(subject.members[:foo]).to be_kind_of(MemberShape)
           end
+
+          it 'can add traits to the member' do
+            subject.add_member(:foo, StringShape.new, traits: { 'trait' => 'value' })
+            expect(subject.members[:foo].traits).to eq({ 'trait' => 'value' })
+          end
         end
       end
 
@@ -124,6 +139,11 @@ module Smithy
             subject.set_member(StringShape.new)
             expect(subject.member).to be_kind_of(MemberShape)
           end
+
+          it 'can set traits on the member' do
+            subject.set_member(StringShape.new, traits: { 'trait' => 'value' })
+            expect(subject.member.traits).to eq({ 'trait' => 'value' })
+          end
         end
       end
 
@@ -135,16 +155,13 @@ module Smithy
         end
 
         describe '#initialize' do
-          context 'key attribute' do
-            it 'defaults to nil' do
-              expect(subject.key).to be(nil)
-            end
+          it 'key defaults to nil' do
+            expect(subject.key).to be(nil)
           end
 
-          context 'member value attribute' do
-            it 'defaults to nil' do
-              expect(subject.value).to be(nil)
-            end
+
+          it 'value defaults to nil' do
+            expect(subject.value).to be(nil)
           end
         end
 
@@ -153,12 +170,22 @@ module Smithy
             subject.set_key(StringShape.new)
             expect(subject.key).to be_kind_of(MemberShape)
           end
+
+          it 'can set traits on the key' do
+            subject.set_key(StringShape.new, traits: { 'trait' => 'value' })
+            expect(subject.key.traits).to eq({ 'trait' => 'value' })
+          end
         end
 
         describe '#set_value' do
           it 'sets a value' do
             subject.set_value(StringShape.new)
             expect(subject.value).to be_kind_of(MemberShape)
+          end
+
+          it 'can set traits on the value' do
+            subject.set_value(StringShape.new, traits: { 'trait' => 'value' })
+            expect(subject.value.traits).to eq({ 'trait' => 'value' })
           end
         end
       end
@@ -171,16 +198,12 @@ module Smithy
         end
 
         describe '#initialize' do
-          context 'members attribute' do
-            it 'defaults to empty hash' do
-              expect(subject.members).to be_empty
-            end
+          it 'defaults members to empty hash' do
+            expect(subject.members).to be_empty
           end
 
-          context 'type attribute' do
-            it 'defaults to nil' do
-              expect(subject.type).to be(nil)
-            end
+          it 'defaults type to nil' do
+            expect(subject.type).to be(nil)
           end
         end
 
@@ -188,6 +211,11 @@ module Smithy
           it 'adds a member' do
             subject.add_member(:foo, StringShape.new)
             expect(subject.members[:foo]).to be_kind_of(MemberShape)
+          end
+
+          it 'can add traits to the member' do
+            subject.add_member(:foo, StringShape.new, traits: { 'trait' => 'value' })
+            expect(subject.members[:foo].traits).to eq({ 'trait' => 'value' })
           end
         end
       end
