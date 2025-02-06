03e769935e08aac58e7ec43e9a8f74ecdebda029
Juli Tera <terajul@amazon.com>
2025-02-06 13:13:59 -0800

    Remove base class

--- gems/smithy-client/lib/smithy-client/protocols.rb
+++ gems/smithy-client/lib/smithy-client/protocols.rb
@@ -2,30 +2,9 @@
 
 module Smithy
   module Client
+    # TODO: Documentation
+    # All the generic protocols
     module Protocols
-      # Base protocol class that all protocols derived from
-      class ProtocolBase
-        def initialize(id)
-          @id = id
-          @service_id = nil
-        end
-
-        attr_reader :id
-
-        attr_accessor :service_id
-
-        def build_request(context)
-          raise NotImplementedError
-        end
-
-        def parse_response(context)
-          raise NotImplementedError
-        end
-
-        def parse_error
-          raise NotImplementedError
-        end
-      end
     end
   end
 end
