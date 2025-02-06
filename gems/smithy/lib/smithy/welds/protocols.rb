37c25081dd59a3032c93401a3e4354a49bbae001
Juli Tera <terajul@amazon.com>
2025-02-06 08:15:01 -0800

    Add protocols weld

---
+++ gems/smithy/lib/smithy/welds/protocols.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'smithy-client/protocols/rpc_v2'
+
+module Smithy
+  module Welds
+    # Provides map of protocol trait id and its Ruby class name.
+    class Protocols < Weld
+      def protocols
+        { 'smithy.protocols#rpcv2Cbor' => 'Smithy::Client::Protocols::RPCv2' }
+      end
+    end
+  end
+end
