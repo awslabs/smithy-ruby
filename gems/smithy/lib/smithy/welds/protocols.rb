2d098e48514eb32d6b853086d53a82f08557da4c
Juli Tera <terajul@amazon.com>
2025-02-06 08:37:12 -0800

    Add more TODO guidance

--- gems/smithy/lib/smithy/welds/protocols.rb
+++ gems/smithy/lib/smithy/welds/protocols.rb
@@ -8,7 +8,9 @@ module Smithy
     # TODO: Update Welds to have a functionality to control ordering since there
     #  is a priority ordered list of protocols and a requirement that SDK MUST
     #  select the first entry in their priority ordered list that is also supported
-    #  by the service. The priority order is as follows (includes AWS protocols):
+    #  by the service. Generic code generation MUST accept configuration of this priority
+    #  priority ordered list for use.
+    #  The priority order is as follows (within AWS context):
     #  * Smithy RPCv2 CBOR
     #  * AWS JSON 1.0
     #  * AWS JSON 1.1
