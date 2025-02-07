d5413e624ed79cb838291a0892ef04f3896028fe
Juli Tera <terajul@amazon.com>
2025-02-07 14:02:01 -0800

    Update protocol modules

--- gems/smithy-client/lib/smithy-client.rb
+++ gems/smithy-client/lib/smithy-client.rb
@@ -41,11 +41,10 @@ require_relative 'smithy-client/endpoint_rules'
 
 # codecs
 require_relative 'smithy-client/cbor'
-require_relative 'smithy-client/codecs/cbor'
+require_relative 'smithy-client/codecs'
 
 # protocols
 require_relative 'smithy-client/protocols'
-require_relative 'smithy-client/protocols/rpc_v2'
 
 module Smithy
   # Base module for a generated Smithy gem.
