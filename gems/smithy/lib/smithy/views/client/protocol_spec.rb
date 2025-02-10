76599942d089333aa3c2e57ac17b7cc02394f071
Alex Woods <alexwoo@amazon.com>
2025-02-10 09:04:32 -0800

    Refactor ShapeToHash utility - not working because of mixins.

--- gems/smithy/lib/smithy/views/client/protocol_spec.rb
+++ gems/smithy/lib/smithy/views/client/protocol_spec.rb
@@ -5,18 +5,90 @@ module Smithy
     module Client
       # @api private
       class ProtocolSpec < View
+        PROTOCOL_TEST_TRAITS = %w[smithy.test#httpRequestTests smithy.test#httpResponseTests].freeze
+
         def initialize(plan)
           @plan = plan
           @model = plan.model
-          service = @plan.service.values.first
-          @operations = Model::ServiceIndex.new(@model).operations_for(@plan.service)
+          @all_operation_tests = Model::ServiceIndex
+                        .new(@model)
+                        .operations_for(@plan.service)
+                        .select { |_id, o| protocol_tests?(o) }
+                        .map { |id, o| OperationTests.new(@model, id, o) }
           super()
         end
+
+        attr_reader :all_operation_tests
 
         def module_name
           @plan.module_name
         end
+        
+        private
+        
+        def protocol_tests?(operation)
+          !!operation.fetch('traits', {}).keys.intersect?(PROTOCOL_TEST_TRAITS)
+        end
 
+        class OperationTests
+          def initialize(model, id, operation)
+            @id = id
+            @operation = operation
+            # TODO: Should we filter protocol tests further by default protocol?
+            # All cases currently have only a single protocol
+            @request_tests = @operation
+                               .fetch('traits', {})
+                               .fetch('smithy.test#httpRequestTests', [])
+                               .select { |t| t['appliesTo'] == 'client' }
+                               .map { |t| RequestTest.new(model, operation, t) }
+            @response_tests = @operation
+                                .fetch('traits', {})
+                                .fetch('smithy.test#httpResponseTests', [])
+                                .select { |t| t['appliesTo'] == 'client' }
+                                .map { |t| ResponseTest.new(model, operation, t) }
+          end
+
+          attr_reader :request_tests, :response_tests
+
+          def name
+            Model::Shape.name(@id).underscore
+          end
+        end
+
+        class RequestTest
+          def initialize(model, operation, test_case)
+            @model = model
+            @operation = operation
+            @test_case = test_case
+            @input_shape = Model.shape(@model, @operation['input']['target'])
+          end
+
+          attr_reader :test_case
+
+          def comments
+            if test_case['documentation']
+              test_case['documentation'].split("\n")
+            else
+              []
+            end
+          end
+
+          def id
+            test_case['id']
+          end
+
+          def params
+            ShapeToHash.transform_value(@model, test_case.fetch('params', {}), @input_shape)
+          end
+        end
+
+        class ResponseTest
+          def initialize(model, operation, test_case)
+            @model = model
+            @operation = operation
+            @test_case = test_case
+          end
+        end
       end
     end
   end
---
+++ gems/smithy/lib/smithy/views/client/shape_to_hash.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+module Smithy
+  module Views
+    module Client
+      # @api private
+      module ShapeToHash
+        class << self
+          def transform_value(model, value, shape)
+            return value unless shape
+
+            case shape['type']
+            when 'structure', 'union'
+              transform_structure(model, shape, value)
+            when 'list'
+              transform_list(model, shape, value)
+            when 'map'
+              transform_map(model, shape, value)
+            else
+              value
+            end
+          end
+
+          private
+
+          def transform_map(model, shape, value)
+            member_shape = Model.shape(model, shape['value']['target'])
+            value.transform_values do |v|
+              transform_value(model, v, member_shape)
+            end
+          end
+
+          def transform_list(model, shape, value)
+            member_shape = Model.shape(model, shape['member']['target'])
+            value.map { |v| transform_value(model, v, member_shape) }
+          end
+
+          def transform_structure(model, shape, value)
+            value.each_with_object({}) do |(k, v), o|
+              if shape['members'].nil? || shape['members'][k].nil?
+                puts "oh no"
+              end
+              member_shape = Model.shape(model, shape['members'][k]['target'])
+              o[k.underscore.to_sym] = transform_value(model, v, member_shape)
+            end
+          end
+        end
+      end
+    end
+  end
+end
