a4ef87cea46b5cc622f872bd262c962f436845a2
Juli Tera <terajul@amazon.com>
2025-02-10 13:27:48 -0800

    update projections

--- projections/weather/lib/weather/plugins/protocol.rb
+++ projections/weather/lib/weather/plugins/protocol.rb
@@ -5,6 +5,7 @@
 module Weather
   module Plugins
     # Protocol plugin - allows user to configure protocol on client.
+    # TODO: Add convenience mapping - see https://github.com/smithy-lang/smithy-ruby/pull/264/files#r1946524403
     # @api private
     class Protocol < Smithy::Client::Plugin
       option(
@@ -14,45 +15,19 @@ module Weather
         docstring: <<~DOCS) do |_cfg|
           Allows you to overwrite default protocol. The given protocol
           must provide the following functionalities:
-          - `build_request`
-          - `parse_response`
-          - `parse_error`
+          - `build`
+          - `parse`
+          - `error`
           See existing protocols within Smithy::Client::Protocols for examples.
         DOCS
         Smithy::Client::Protocols::RPCv2.new
       end
 
-      # @api private
-      class Build < Smithy::Client::Handler
-        def call(context)
-          context.config.protocol.build_request(context)
-          @handler.call(context)
-        end
-      end
-
-      # @api private
-      class Parse < Smithy::Client::Handler
-        def call(context)
-          resp = @handler.call(context)
-          context.config.protocol.parse_response(context)
-          resp
-        end
-      end
-
-      # @api private
-      class Error < Smithy::Client::Handler
-        def call(context)
-          @handler.call(context).on(300..599) do |response|
-            context.config.protocol.parse_error(context, response)
-          end
-        end
-      end
-
       def add_handlers(handlers, _config)
-        handlers.add(Build)
-        handlers.add(Parse)
+        handlers.add(Smithy::Client::Handlers::Build)
+        handlers.add(Smithy::Client::Handlers::Parse)
         # TODO: Requires error handling to be implemented
-        # handlers.add(Error, step: :sign)
+        # handlers.add(Smithy::Client::Handlers::Error, step: :sign)
       end
     end
   end
